17ac8d9d157b4598aa29a7753e2c6cf3888bf707
SMN-web <nafil.8895.s@gmail.com>
2025-08-06 16:55:32 +0300

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-export function showPanel(panelId) {
-  document.querySelectorAll(".panel").forEach(el => el.classList.add("hidden"));
-  document.getElementById(panelId)?.classList.remove("hidden");
-}
-
-export function logout() {
-  sessionStorage.clear();
-  showPanel("login-panel");
-}
