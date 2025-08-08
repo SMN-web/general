abad99f3379f39fd718e4cc8f7a61f005356fa06
SMN-web <nafil.8895.s@gmail.com>
2025-08-08 20:06:07 +0300

    Update moderator.js

--- moderator.js
+++ moderator.js
@@ -1,12 +1,9 @@
 import { setupLogoutHandler, startHeartbeat } from "./main.js";
   
-  
 export function initModeratorPanel() {
   const email = sessionStorage.getItem("loggedInEmail");
-  document.getElementById("moderator-panel").innerHTML = `
-    <h2>🛡️ Moderator Dashboard</h2>
-    <p>Welcome moderator, ${email}.</p>
-  `;
-   setupLogoutHandler();
-   startHeartbeat();
+  document.getElementById("moderator-email").textContent = email;
+
+  setupLogoutHandler();
+  startHeartbeat();
 }
