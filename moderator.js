0f26f8f987b60cfa60442f63bab3e5817103246b
SMN-web <nafil.8895.s@gmail.com>
2025-08-08 16:05:52 +0300

    Update moderator.js

--- moderator.js
+++ moderator.js
@@ -2,10 +2,10 @@ import { setupLogoutHandler } from "./main.js";
   
   
 export function initModeratorPanel() {
-  setupLogoutHandler();
   const email = sessionStorage.getItem("loggedInEmail");
   document.getElementById("moderator-panel").innerHTML = `
     <h2>🛡️ Moderator Dashboard</h2>
     <p>Welcome moderator, ${email}.</p>
   `;
+   setupLogoutHandler();
 }
