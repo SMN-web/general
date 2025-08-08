cffcd727e0a0fce2c72549ab001abbc28f6be62e
SMN-web <nafil.8895.s@gmail.com>
2025-08-08 15:56:14 +0300

    Update users.js

--- users.js
+++ users.js
@@ -1,4 +1,8 @@
+import { setupLogoutHandler } from "./main.js";
+
+  
 export function initUserPanel() {
+  setupLogoutHandler();
   const email = sessionStorage.getItem("loggedInEmail");
   document.getElementById("user-panel").innerHTML = `
     <h2>👤 User Dashboard</h2>
