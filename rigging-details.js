5d39a57913cec1b5e89c465690b9c4ba491316e9
SMN-web <nafil.8895.s@gmail.com>
2025-08-09 15:25:02 +0300

    Update rigging-details.js

--- rigging-details.js
+++ rigging-details.js
@@ -1,5 +1,3 @@
-// rigging-details.js
 export function initRiggingDetails() {
-  console.log("🪝 Rigging Details section loaded (placeholder)");
-  // In the future, load rigging info here
+  console.log("🪝 Rigging Details initialized");
 }
