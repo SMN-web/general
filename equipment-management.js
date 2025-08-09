93bc65ec5d2bb8bb63e99fa63d4394a1e885def8
SMN-web <nafil.8895.s@gmail.com>
2025-08-09 14:44:17 +0300

    Create equipment-management.js

---
+++ equipment-management.js
@@ -0,0 +1,5 @@
+// equipment-management.js
+export function initEquipmentManagement() {
+  console.log("⚙ Equipment Management section loaded (placeholder)");
+  // In the future, load equipment list or controls here
+}
