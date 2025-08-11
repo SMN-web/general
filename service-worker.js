71c7d4d0e5bf3323bd18cccd70692a9c091b2189
SMN-web <nafil.8895.s@gmail.com>
2025-08-11 19:08:24 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,9 +2,20 @@ self.addEventListener('push', e => {
   let data = {};
   if (e.data) data = e.data.json();
   e.waitUntil(
-    self.registration.showNotification(data.title || 'Hi', {
-      body: data.body || '',
-      icon: 'icon.png'
-    })
+    self.registration.showNotification(
+      data.title || 'Notification',
+      {
+        body: data.body || '',
+        icon: data.icon || 'icon.png',
+        data: data
+      }
+    )
+  );
+});
+
+self.addEventListener('notificationclick', event => {
+  event.notification.close();
+  event.waitUntil(
+    clients.openWindow('/')
   );
 });
