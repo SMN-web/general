860cba102b9940e6dab4b7a310daec3b4df73130
SMN-web <nafil.8895.s@gmail.com>
2025-08-12 09:10:09 +0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -3,9 +3,12 @@ importScripts('https://www.gstatic.com/firebasejs/9.22.0/firebase-messaging-comp
 
 firebase.initializeApp({
   apiKey: "AIzaSyDtZnYYDb5TR01G6zsCtrF0HBR6pnQ2Beg",
-  projectId: "general-68ca7",
-  messagingSenderId: "674522865143",
-  appId: "1:674522865143:web:c4ec47f2e370c33c3ca2f2"
+    authDomain: "general-68ca7.firebaseapp.com",
+    projectId: "general-68ca7",
+    storageBucket: "general-68ca7.firebasestorage.app",
+    messagingSenderId: "674522865143",
+    appId: "1:674522865143:web:c4ec47f2e370c33c3ca2f2",
+    measurementId: "G-6L0DXHGCBE"
 });
 
 const messaging = firebase.messaging();
