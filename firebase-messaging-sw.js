ca17a2d610c8557a0c3fc00fddcb8d95a4c903ae
SMN-web <nafil.8895.s@gmail.com>
2025-08-12 08:09:43 +0300

    Create firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,18 @@
+importScripts('https://www.gstatic.com/firebasejs/9.22.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.22.0/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDtZnYYDb5TR01G6zsCtrF0HBR6pnQ2Beg",
+  projectId: "general-68ca7",
+  messagingSenderId: "674522865143",
+  appId: "1:674522865143:web:c4ec47f2e370c33c3ca2f2"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(payload => {
+  self.registration.showNotification(payload.notification.title, {
+    body: payload.notification.body,
+    icon: payload.notification.icon || '/icon.png'
+  });
+});
