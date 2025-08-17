9b0195ac53ab3925d8ab8a7073beca0757cf1bf2
SMN-web <nafil.8895.s@gmail.com>
2025-08-17 06:44:34 +0300

    Update crane-status.js

--- crane-status.js
+++ crane-status.js
@@ -12,7 +12,7 @@ export function initCraneStatus() {
     form.querySelector('.description').value = '';
     form.querySelector('.reason').value = '';
     form.querySelector('.date').value = '';
-    actionSelect.value = 'breakdown';
+    actionSelect.value = 'None';
     document.getElementById('crane-reason-block').style.display = 'block';
   }
 
